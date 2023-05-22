d682a603418c24197640e1f0d00d9013026f76fc
ambarja <antony.barja8@gmail.com>
2023-05-22 05:42:34 -0500

    updaed

---
+++ utils.R
@@ -0,0 +1,26 @@
+img_get_value <- function(img){
+  download.img <- img |> 
+    ee_as_stars(
+      region = peru_ee,
+      scale = 5*1000
+    )
+  extract.value <- download.img |> 
+    st_extract(Peru,fun = "mean") |> 
+    st_as_sf() |> 
+    mutate(ubigeo = Peru$ubigeo) |> 
+    st_drop_geometry()
+  
+  tidy.db <- extract.value |> 
+    pivot_longer(
+      cols = -one_of("ubigeo"),
+      names_to = "date",
+      values_to = "value"
+    ) |>
+    mutate(
+      variable = str_extract(pattern = "[Aa-zZ]+",date), 
+      date = ymd(str_extract(pattern = "[0-9]+",date))
+    ) 
+  if(dir.exists("output")!=1){dir.create("output")}
+  write_csv(tidy.db,sprintf("output/%s.csv",unique(tidy.db[["variable"]])))
+  return(tidy.db)
+}
