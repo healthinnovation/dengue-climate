ee6de6beb2db9ac39bc9bd82c26f85f56d5a1967
Diego Villa <ddevildiego@gmail.com>
2023-06-14 13:13:21 -0500

    Processed complete datasets

--- scripts/03-process-climate.R
+++ scripts/03-process-climate.R
@@ -1,28 +1,39 @@
 file_paths = fs::dir_ls("data/raw/climate/")
 datasets = purrr::map(
-  file_paths, \(x) readr::read_csv(x, col_select = 1:3, col_types = "cDd")
+  file_paths,
+  \(x) readr::read_csv(
+    x, col_select = c(ubigeo, date, value), col_types = "iccccDdc"
+  )
 )
 names(datasets) = fs::path_file(fs::path_ext_remove(names(datasets)))
 
 library(dplyr)
 
-weekly = function(data) {
-  data_weekly = data |>
-    mutate(
-      week_start = lubridate::floor_date(date, unit = "week", week_start = 7),
-      week = lubridate::epiweek(date),
-      year = lubridate::epiyear(date)
-    ) |>
-    group_by(ubigeo, year, week_start, week) |>
-    summarise(value = mean(value), .groups = "drop")
-  data_weekly
-}
-
-weekly_datasets = purrr::map(datasets, weekly)
-weekly_dataset = purrr::reduce(
-  weekly_datasets, inner_join, by = c("ubigeo", "year", "week_start", "week")
+dataset = purrr::reduce(
+  datasets, inner_join, by = c("ubigeo", "date")
 )
-names(weekly_dataset)[-1:-(length(datasets) + 1)] = names(datasets)
+n_col = ncol(dataset)
+n_vars = length(datasets)
+names(dataset)[(n_col - n_vars + 1):n_col] = names(datasets)
+
+naniar::miss_var_summary(dataset)
+
+dataset |>
+  group_by(ubigeo) |>
+  summarise(n = n_distinct(date))
+
+weekly_dataset = dataset |>
+  mutate(
+    week_start = lubridate::floor_date(date, unit = "week", week_start = 7),
+    week = lubridate::epiweek(date),
+    year = lubridate::epiyear(date)
+  ) |>
+  group_by(ubigeo, year, week_start, week) |>
+  summarise(across(max_evapo:min_temp, \(x) mean(x)), .groups = "drop") |>
+  rename_with(
+    \(x) paste0("climate_", stringr::str_replace_all(x, "_", "")),
+    .cols = max_evapo:min_temp
+  )
 
 output_path = "data/interim/climate.csv"
 readr::write_csv(weekly_dataset, output_path, na = "")
--- scripts/05-process-sociodemographic.R
+++ scripts/05-process-sociodemographic.R
@@ -1,24 +1,46 @@
-census_raw = readxl::read_excel(
-  "data/raw/census-data.xlsx", range = "B6:AI1880", col_names = TRUE
+sociodemo_colnames = c(
+  "ubigeo", "district", "population", "non_precarious_household",
+  "public_water_supply", "street_lighting", "have_fridge", "urban", "rural",
+  "men", "women", "have_insurance", "literate"
+)
+
+sociodemo_raw = readr::read_csv(
+  "data/raw/sociodemographic.csv", col_names = sociodemo_colnames,
+  col_types = "cccddddddddddd", skip = 1
 )
 
 library(dplyr)
 
-census = census_raw |>
-  mutate(ubigeo = stringr::str_pad(`Código`, 6, pad = "0")) |>
-  rename(rural = `Rural encuesta`, urban = `Urbano encuesta`) |>
-  mutate(rural_pct = rural / (rural + urban)) |>
-  select(ubigeo, rural_pct)
+sociodemo = sociodemo_raw |>
+  select(ubigeo, non_precarious_household:literate) |>
+  mutate(
+    ubigeo = stringr::str_pad(ubigeo, width = 6, side = "left", pad = "0")
+  ) |>
+  rename_with(
+    \(x) paste0("socio_", stringr::str_replace_all(x, "_", "")), .cols = -ubigeo
+  )
 
-socioeconomic_raw = readr::read_csv(
-  "data/raw/socioeconomic-data.csv", col_types = "cddddddddddddd"
+socioeco_raw = readr::read_csv(
+  "data/raw/socioeconomic.csv", col_types = "cddddddddddddd"
 )
 
-socioeconomic = socioeconomic_raw |>
-  mutate(nbi = hh_1_nbi_or_more / number_hh) |>
-  select(ubigeo, monetary_poverty, nbi)
+socioeco = socioeco_raw |>
+  mutate(
+    ubn = 100 * hh_1_nbi_or_more / number_hh,
+    inadequate = 100 * hh_inadequate_char / number_hh,
+    overcrowded = 100 * overcrowded_hh / number_hh,
+    nosanitation = 100 * hh_wo_sanitation / number_hh,
+    schoolabscence = 100 * hh_school_absence / number_hh,
+    highecodependence = 100 * hh_high_economic_dependence / number_hh,
+    monetarypoverty = 100 * monetary_poverty,
+    completesecondaryedu = 100 * complete_secondary_edu,
+    .keep = "unused"
+  ) |>
+  rename_with(
+    \(x) paste0("socio_", stringr::str_replace_all(x, "_", "")), .cols = -ubigeo
+  )
 
-sociodemographic = socioeconomic |>
-  inner_join(census, by = "ubigeo")
+socio = sociodemo |>
+  inner_join(socioeco, by = "ubigeo")
 
-readr::write_csv(sociodemographic, "data/interim/sociodemographic.csv")
+readr::write_csv(socio, "data/interim/sociodemographic.csv")
